0ffd828f3ee0e15342ecde60443582465b575e8a
mohitmalviya0707 <mohitmalviya0107@gmail.com>
2026-02-28 14:03:34 +0530

    Delete supabase directory

--- supabase/migrations/20260228035015_d5dbe06f-3a3c-47e2-b8f0-12e35fb78189.sql
+++
@@ -1,6 +0,0 @@
-
-DROP POLICY IF EXISTS "Anyone can read registrations" ON public.registrations;
-DROP POLICY IF EXISTS "Anyone can register" ON public.registrations;
-
-CREATE POLICY "Anyone can read registrations" ON public.registrations FOR SELECT USING (true);
-CREATE POLICY "Anyone can register" ON public.registrations FOR INSERT WITH CHECK (true);
